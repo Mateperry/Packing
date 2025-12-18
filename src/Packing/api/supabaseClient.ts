958249037230911ed7391c8e60749e1c680a73b5
Pasante TI 5 <pasante.ti5@sumimas.local>
2025-12-18 07:41:39 -0500

    Actualizar

--- src/Packing/api/supabaseClient.ts
+++ src/Packing/api/supabaseClient.ts
@@ -1,10 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Tus credenciales de Supabase
+const supabaseUrl = "https://wdyukwekpemmzkcaaple.supabase.co";
+const supabaseAnonKey = "sb_publishable_JjQNJ-TKhVPLQTCoWswkUw_lbzoqlQF";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("VITE_SUPABASE_URL y VITE_SUPABASE_ANON_KEY deben estar definidas en el .env");
 }
 
+
+
+
+// Crear cliente de Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
